62adb7d4630abf1431271a2fad83b182050a01a4
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-01-13 16:11:20 +0100

    Update task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const items = document.querySelectorAll("li.item");
+console.log("Number od categories: " + items.length);
+
+const categories = items.querySelector("h2").textContent;
+console.log(items);
+
+const liItems = item.querySelectorAll("ul>li");
+console.log(liItems);
